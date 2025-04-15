f662352e710dac05d6571cdddf0345493fd6704f
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-04-15 14:01:45 +0200

    Windows update
    First conversion of the linux script to Windows

---
+++ Project_Model/Scripts/Python/main_dos.py
@@ -0,0 +1,17 @@
+from Project_Model.Scripts.Python.run_simulation_dos import run_simulation_dos  # Name of the simulation script
+import argparse
+import time
+
+# --- Argparse module ---
+# This part allows the simulation to be run n times
+parser = argparse.ArgumentParser() # Convert to a command line
+parser.add_argument(  # Create the argument that will be converted
+    "-n", "--num_simulations",
+    type=int, 
+    default=10,  # Number of simulations if nothing is specified
+)
+args = parser.parse_args()
+
+for i in range(args.num_simulations):  # 10 simulations
+    run_simulation_dos()  # Python script 
+    time.sleep(0.5)  # Latency between every simulation, necessary for RAM purpose
