6b3760980be0fed124416a01cfe4725d2275de0c
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-04-22 14:51:26 +0200

    Corrections

--- Project_Model/Scripts/Python/run_simulation_linux.py
+++ Project_Model/Scripts/Python/run_simulation_linux.py
@@ -467,7 +467,7 @@ def run_simulation_linux():
                 "LD_Ne_Pop1", "LD_r2_Pop1", "HE_Neb_mean_Pop1", "HE_weighted_D_mean_Pop1", "Coan_Neb_n_Pop1", "Coan_f1_Pop1",
                 "LD_Ne_Pop2", "LD_r2_Pop2", "HE_Neb_mean_Pop2", "HE_weighted_D_mean_Pop2", "Coan_Neb_n_Pop2", "Coan_f1_Pop2" 
             ], f)
-        write_section("Ne Estimates - Temporal - Decreasing critical values [0.050, 0.020, 0.010, 0+]" ["Pollak_Ne", "Nei/Tajima_Ne", "Jorde/Ryman_Ne"], f)
+        write_section("Ne Estimates - Temporal - Decreasing critical values [0.050, 0.020, 0.010, 0+]", ["Pollak_Ne", "Nei/Tajima_Ne", "Jorde/Ryman_Ne"], f)
         write_section("Genetic Diversity - Heterozygosity", [
                 "mean_exp_het_pop1", "mean_obs_het_pop1",
                 "mean_exp_het_pop2", "mean_obs_het_pop2"
