3ee87123362c6f4eda5bb9a928798742efd697e4
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-04-23 10:33:46 +0200

    Correction

--- Project_Model/Scripts/Python/run_simulation_linux.py
+++ Project_Model/Scripts/Python/run_simulation_linux.py
@@ -46,8 +46,9 @@ def run_simulation_linux(base_dir="simulations", pop_size=None, num_loci=None, s
         with open(global_config_path, "r") as f:
             for line in f:
                 if "=" in line and not line.strip().startswith("#"):
-                    key, value = line.strip().split("=", 1)
-                    config[key.strip()] = value.strip()
+                    raw_key, value = line.strip().split("=", 1)
+                    key = inverse_better_names.get(raw_key.strip(), raw_key.strip())
+                    config[key] = value.strip()
         return config
 
     ### 2.1. Directory script ###
