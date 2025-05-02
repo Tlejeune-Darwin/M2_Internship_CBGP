bc0c1d8fe3a783662bd6f5126bfccff7d9bb769e
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-05-02 15:11:22 +0200

    Update launch_all.sh

--- Project_Model/Scripts/Bash/launch_all.sh
+++ Project_Model/Scripts/Bash/launch_all.sh
@@ -5,6 +5,7 @@ NB_BATCHES=2
 SIMS_PER_BATCH=10
 MAX_CONCURRENT=10   # Nombre max de jobs en simultané
 
+
 # Dossier contenant le script SLURM
 SCRIPT_DIR="$HOME/M2_Internship_CBGP/Project_Model/Scripts/Bash"
 
@@ -12,6 +13,9 @@ for i in $(seq -w 0 $((NB_BATCHES - 1))); do
     BATCH_NAME="batch_$i"
     OFFSET=$((10#$i * SIMS_PER_BATCH))
     
-    sbatch --export=BATCH=$BATCH_NAME,OFFSET=$OFFSET,SIMS=$SIMS_PER_BATCH,MAX=$MAX_CONCURRENT \
-           "$SCRIPT_DIR/job_run_batch.slurm"
+    ARRAY_MAX=$((SIMS_PER_BATCH - 1))
+    sbatch --array=0-${ARRAY_MAX}%${MAX_CONCURRENT} \
+        --export=BATCH=$BATCH_NAME,OFFSET=$OFFSET \
+        "$SCRIPT_DIR/job_run_batch.slurm"
+
 done
