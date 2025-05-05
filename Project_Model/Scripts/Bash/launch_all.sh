9a4b38b0cea81f2a6a9a46def2ef667bc3387ee9
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-05-05 11:14:11 +0200

    Update launch_all.sh

--- Project_Model/Scripts/Bash/launch_all.sh
+++ Project_Model/Scripts/Bash/launch_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Nombre de batchs
-TOTAL_BATCHES=4
+TOTAL_BATCHES=4x
 SIMS_PER_BATCH=10
 MAX_JOBS=1
 
 for i in $(seq 0 $((TOTAL_BATCHES - 1))); do
-    BATCH_NAME="batch_${i}"
+    BATCH_NAME="batch_ref_${i}"
     OFFSET=$((i * SIMS_PER_BATCH))
 
         # Tant qu'on a déjà trop de jobs en attente ou en cours...
-    while [ $(squeue -u "lejeunet" -h -o "%j" | grep -c "^batch_") -ge $MAX_JOBS ]; do
+    while [ $(squeue -u lejeunet -h -o "%j" | grep -c "^batch_") -ge $MAX_JOBS ]; do
         echo "⏳ $MAX_JOBS jobs déjà en cours. Attente..."
         sleep 20
     done
