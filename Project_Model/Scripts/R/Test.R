d95b691d79356c5f20e12c5d675db05c96d8f056
Tlejeune-Darwin <thomas.lejeune.eco@gmail.com>
2025-05-06 14:47:43 +0200

    Commit

--- Project_Model/Scripts/R/Test.R
+++ Project_Model/Scripts/R/Test.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(abcrf)
 
 # Read the file
-ref_data <- read_csv("Bureau/simulations/Ref_table/summary_table.csv")
+ref_data <- read_csv("Bureau/simulations/summary_table_all_batches.csv")
 
 params <- ref_data[, c("simulation_id", "pop_size", "num_loci", "sample1_size_Ne", "sample2_size_Ne", "sample1_size_CMR", "sample2_size_CMR", "mutation_rate", "recap_Ne")]
 stats_keywords <- c("id", "LD", "HE", "Coan", "het", "alleles", "P", "N", "J")
