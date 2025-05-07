7d84f9728d9493e9aa5676429bb95f9d5dd1fe44
PlasmaPulsar <thomas.lejeune.eco@gmail.com>
2025-05-07 10:46:20 +0200

    Update merge_summary.py

--- Project_Model/Scripts/Python/merge_summary.py
+++ Project_Model/Scripts/Python/merge_summary.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd # type: ignore
 
-base_results_dir = os.path.expanduser("~/results")
+base_results_dir = os.path.expanduser("~/results/table_ref_2")
 batch_dirs = sorted([d for d in os.listdir(base_results_dir) if d.startswith("batch_table_ref_")])
 
 all_df = []
